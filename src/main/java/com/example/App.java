3c2aca3a36b7638c8fc5354325966f373c27afb4
Howl0921 <howllee.cn@gmail.com>
2024-12-29 21:19:39 +0800

    Second Commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Jenkins!" );
+        System.out.println( "this is second commit!! Jenkins is amazing!!!" );
+
     }
 }
